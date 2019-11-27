4f34354c0ee0a4667de0ba4d5acb474fc8784771
mdecourse <mdecourse@gmail.com>
2019-11-27 10:51:35 +0800

    加入批次檔頁面以及 Webots one-link Solvespace imported wrl 模擬範例

--- downloads/solvespace/one_link/controllers/onelink/onelink.py
+++ downloads/solvespace/one_link/controllers/onelink/onelink.py
@@ -6,9 +6,9 @@ TIME_STEP = 64
 robot = Robot()
 
 # get the motor devices
-motor = robot.getMotor('motor')
+motor = robot.getMotor('rotational motor')
 # set the target position of the motors
-motor.setPosition(10.0)
+motor.setPosition(100.0)
 
 while robot.step(TIME_STEP) != -1:
    pass
